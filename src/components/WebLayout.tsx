f4f1089175e68dddb2b3745273abec2db8268f85
Javier <javier.lopez@lendis.de>
2023-01-17 16:46:54 +0100

    Update texts

--- src/components/WebLayout.tsx
+++ src/components/WebLayout.tsx
@@ -8,11 +8,11 @@ const WebLayout: React.FC<WebLayoutProps> = ({ children }) => {
   return (
     <div className="flex flex-col h-screen dark:bg-slate-900 bg-slate-50">
       <header className="text-2xl font-bold h-12 dark:text-slate-200 text-slate-900 flex justify-center items-center">
-        Norigin Media (web)
+        Norigin Media (mobile and desktop web)
       </header>
       <main className="grow">{children}</main>
       <footer className="text-center h-12 dark:text-slate-200 text-slate-900">
-        Footer
+        Made with ❤️ in Spain
       </footer>
     </div>
   );
